ee376f57b4bf69d010c14e6f3afbe0b7268e055b
Jelmer <kanisjelmer@gmail.com>
2023-11-24 12:32:54 +0100

    Bezig geweest met popup voor de winkelmand - popup.js toegevoegd

--- cart.php
+++ cart.php
@@ -5,13 +5,57 @@ $dbConnection = connectToDatabase();
 ?>
 <!DOCTYPE html>
 <html lang="nl">
+<style>
+    /* Style for the pop-up container */
+    .popup {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.5); /* semi-transparent background */
+        display: none;
+    }
 
+    /* Style for the pop-up content */
+    .popup-content {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: #fff;
+        padding: 20px;
+        border-radius: 5px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+        text-align: center;
+    }
 
+    /* Style for the close button */
+    .close {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        font-size: 20px;
+        cursor: pointer;
+    }
+
+    /* Style for the trigger button */
+    .openPopup {
+        display: block;
+        margin: 20px auto;
+        padding: 10px;
+        font-size: 16px;
+        cursor: pointer;
+    }
+</style>
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Winkelwagen</title>
+    <script src='Popup.js'></script>
 </head>
 <body>
+
 <h1>Inhoud Winkelwagen</h1>
 <table>
     <tr>
@@ -19,20 +63,32 @@ $dbConnection = connectToDatabase();
         <th>Prijs</th>
         <th>Aantal</th>
     </tr>
+
     <?php
 
-    function HelloConflict()
-    {
-        print("I'm a merge conflict!");
-    }
     
     $cart = getCart();
+    $popUpHtml = " <!-- Trigger/Open The Modal -->
 
+
+<div id='popupContainer' class='popup'>
+    <!-- Pop-up content -->
+    <div class='popup-content'>
+        <span class='close' id='closePopup'>&times;</span>
+        <h2>Hello, this is a pop-up!</h2>
+        <p>This is some content inside the pop-up.</p>
+    </div>
+</div>";
+
+//    $popUpConfirm = 1;
     if(isset($_POST['removeform']))
     {
-        $removeID = $_POST['product'];
-        removeProductFromCart($removeID);
-        $cart = getCart();
+        print($popUpHtml);
+//        if($popUpConfirm == TRUE) {
+//            $removeID = $_POST['product'];
+//            removeProductFromCart($removeID);
+//            $cart = getCart();
+//        }
     }
 
     if(isset($_POST['modifiedAmount']))
@@ -87,7 +143,7 @@ $dbConnection = connectToDatabase();
          <th>
             <form method='post' action='cart.php'>
                 <input type='hidden' name='product' value='$itemId'>
-                <input type='submit' name='removeform' id='REMOVE' value='X'>
+                <input type='submit' name='removeform' id='REMOVE' value='X' onclick=''>
             </form>
          </th>
         </tr>";
@@ -104,12 +160,6 @@ $dbConnection = connectToDatabase();
         </tr>
         ";
     print($totalPriceHTML);
-    
-    //print_r($cart);
-    //gegevens per artikelen in $cart (naam, prijs, etc.) uit database halen
-    //totaal prijs berekenen
-    //mooi weergeven in html
-    //etc.
 
     ?>
 
@@ -119,5 +169,18 @@ $dbConnection = connectToDatabase();
 <button style="background-color:#676EFF; border-radius: 8px; color: white; padding: 10px 20px; font-family: vortice-concept, sans-serif; font-weight: bold; position:relative; left:1300px; top:2px">BESTELLEN</button>
 </a>
 </div>
+<button id="openPopup" class="openPopup">Open Pop-up</button>
+
+<!-- The pop-up container -->
+<div id="popupContainer" class="popup">
+    <!-- Pop-up content -->
+    <div class="popup-content">
+        <span class="close" id="closePopup">&times;</span>
+        <h2>Hello, this is a pop-up!</h2>
+        <p>This is some content inside the pop-up.</p>
+    </div>
+</div>
+
+<script src="Popup.js"></script>
 </body>
 </html>
--- cartfuncties.php
+++ cartfuncties.php
@@ -30,6 +30,8 @@ function addProductToCart($stockItemID)
     saveCart($cart);                            // werk de "gedeelde" $_SESSION["cart"] bij met de bijgewerkte cart
 }
 
+
+
 function removeProductFromCart($stockItemID)
 {
     $cart = getCart();
