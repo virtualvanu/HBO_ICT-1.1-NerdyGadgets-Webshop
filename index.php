c5bbc52db39761aa1376a043f4e59ad748b1997e
Peter Bos <s1200965@st.windesheim.nl>
2023-12-12 16:07:34 +0100

    Stared work on adding top selling products to index.php. Created HTML/CSS mock-up without functionality

--- index.php
+++ index.php
@@ -16,7 +16,57 @@ include __DIR__ . "/header.php";
         </a>
         <div class="HomePageStockItemPicture"></div>
     </div>
+    <div class="col-xl-11" style="text-align: center;">
+        <h1 class="TextMain" style="font-size: xx-large; text-align: -moz-center;"><b>TOP SELLERS</b></h1>
+        <table class="col-xl-11" style="font-size: xx-large; text-align: -moz-center;">
+            <tr>
+                <th>#1</th>
+                <th>#2</th>
+                <th>#3</th>
+                <th>#4</th>
+                <th>#5</th>
+            </tr>
+            <tr>
+                <th>
+                    <h3 class="TopSellerProductName">Furry animal socks (Pink) S</h3>
+                    <img src="Images/Placeholder.svg" class="TopSellerImage">
+                    <p class="TopSellerPrice">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+                    <p class="TopSellerPrice">€999.99</p>
+                </th>
+                <th>
+                    <h3 class="TopSellerProductName">RC toy sedan car with remote control (Blue) 1/50 scale</h3>
+                    <img src="Images/Placeholder.svg" class="TopSellerImage">
+                    <p class="TopSellerPrice">€100.99!</p>
+                    <span style='color:red;text-decoration:line-through ' class="TopSellerPrice" >
+                        <span style='color:#5F63A5'>€999.99</span>
+                    </span>
+                </th>
+                <th>
+                    <h3 class="TopSellerProductName">DBA joke mug - mind if I join you? (Black)</h3>
+                    <img src="Images/Placeholder.svg" class="TopSellerImage">
+                    <p class="TopSellerPrice">€100.99!</p>
+                    <span style='color:red;text-decoration:line-through ' class="TopSellerPrice" >
+                        <span style='color:#5F63A5'>€999.99</span>
+                    </span>                </th>
+                <th>
+                    <h3 class="TopSellerProductName">"The Gu" red shirt XML tag t-shirt (White) S</h3>
+                    <img src="Images/Placeholder.svg" class="TopSellerImage">
+                    <p class="TopSellerPrice">€100.99!</p>
+                    <span style='color:red;text-decoration:line-through ' class="TopSellerPrice" >
+                        <span style='color:#5F63A5'>€999.99</span>
+                    </span>                </th>
+                <th>
+                    <h3 class="TopSellerProductName">White chocolate moon rocks 250g</h3>
+                    <img src="Images/Placeholder.svg" class="TopSellerImage">
+                    <p class="TopSellerPrice">€100.99!</p>
+                    <span style='color:red;text-decoration:line-through ' class="TopSellerPrice" >
+                        <span style='color:#5F63A5'>€999.99</span>
+                    </span>                </th>
+            </tr>
+        </table>
+    </div>
 </div>
+
 <?php
 include __DIR__ . "/footer.php";
 ?>
